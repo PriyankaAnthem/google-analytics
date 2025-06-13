54e443918fb7775332ba666934fc5d4230204db3
Priyanka <priyanka@antheminfotech.com>
2025-06-13 14:40:30 +0530

    ..

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,16 @@
 "use client"
 export default function Home() {
+    const handleClick = () => {
+    if (typeof window.gtag !== "undefined") {
+      window.gtag("event", "click", {
+        event_category: "button",
+        event_label: "CTA Button",
+        value: 1,
+      });
+    }
+    // Your normal click behavior
+    alert("Button clicked!");
+  };
  
   return (
 
@@ -7,7 +18,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors" onClick={() => alert('Button clicked!')}>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors" onClick={handleClick()}>
             Click me!
           </button>
         </div>
