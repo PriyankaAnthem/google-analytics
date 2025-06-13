62e8e25b00fb1d4c55c23684dca5d985b4e39f7f
Priyanka <priyanka@antheminfotech.com>
2025-06-13 14:42:37 +0530

    ..

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors" onClick={handleClick()}>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors" onClick={handleClick}>
             Click me!
           </button>
         </div>
